3d85c590aafd3de6508340e8b675693891f9011c
Elisha Adeboye <y.adeboye@gmail.com>
2025-02-11 17:45:57 +0100

    made step one of the form responsive

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -8,11 +8,12 @@ const StepOne = () => {
         <p>
           Join us for an unforgettable experience at <br />
           [Event name]! Secure your space now <br />
+          <br />
           📍[lEvent location] <span className="seperator">| |</span> March 15,
           2025<span className="seperator">|</span>7:00 PM
         </p>
       </div>
-
+      <br />
       <hr />
 
       <section>
@@ -20,24 +21,24 @@ const StepOne = () => {
         <div className="ticket_type_card">
           <div className="access_card">
             <div className="access_wrap">
-              <p>REGULAR ACCESS</p>
-              <span>25 left</span>
+              <p className="access_price">Free</p>
+              <p className="ticket_type">REGULAR ACCESS</p>
+              <span className="ticket_type">20/52</span>
             </div>
-            <span className="access_price">Free</span>
           </div>
           <div className="access_card">
             <div className="access_wrap">
-              <p>VIP ACCESS</p>
-              <span>25 left</span>
+              <p className="access_price">$50</p>
+              <p className="ticket_type">VIP ACCESS</p>
+              <span className="ticket_type"> 20/52</span>
             </div>
-            <span className="access_price">$50</span>
           </div>
           <div className="access_card">
             <div className="access_wrap">
-              <p>VVIP ACCESS</p>
-              <span>25 left</span>
+              <p className="access_price">$150</p>
+              <p className="ticket_type">VVIP ACCESS</p>
+              <span className="ticket_type">20/52</span>
             </div>
-            <span className="access_price">$150</span>
           </div>
         </div>
 
