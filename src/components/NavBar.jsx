47308886c6b2932b7e11cf8114bc450d996f0b98
Elisha Adeboye <y.adeboye@gmail.com>
2025-02-15 13:40:26 +0100

    edited word

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import logoticket from "../assets/logoticket.png";
 import btn_ico from "../assets/btn_ico.png";
 import { Link } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const NavBar = () => {
+  const { step, setStep, formData, setFormData } = useContext(AppContext);
   return (
     <div className="nav">
       <span className="logodiv">
@@ -14,7 +16,7 @@ const NavBar = () => {
         <Link>My Tickets</Link>
         <Link>About Project</Link>
       </span>
-      <span className="nav_btn_wrapper">
+      <span className="nav_btn_wrapper" onClick={() => setStep(1)}>
         <button className="btn_nav">
           MY TICKETS
           <img src={btn_ico} alt="-" />
